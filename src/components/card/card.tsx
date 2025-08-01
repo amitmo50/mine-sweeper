e0b9dc1c991bc7d2091e03969b6eb495c6de4a37
amitmo50 <amitmo50@gmail.com>
2025-08-02 00:22:58 +0300

    fix animation

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CardProps } from "../../app.types";
 import { getTileContent } from "../../utils/utils";
 import { cn } from "../../utils/cn";
 import { baseClasses } from "./card.consts";
 
 export const Card = ({ tile, onClick, onRightClick, gameOver }: CardProps) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    if (tile.isRevealed && !isFlipped) {
+      setIsFlipped(true);
+    }
+  }, [tile.isRevealed, isFlipped]);
+
   const handleRightClick = (e: React.MouseEvent) => {
     e.preventDefault();
     onRightClick();
@@ -15,18 +23,20 @@ export const Card = ({ tile, onClick, onRightClick, gameOver }: CardProps) => {
   const isMine = tile.isMine;
 
   const getTileClasses = () => {
+    const flipClasses = isFlipped ? "card-flip" : "";
+
     if (isFlagged) {
-      return cn(baseClasses, "bg-yellow-100 hover:bg-yellow-200");
+      return cn(baseClasses, "bg-yellow-100 hover:bg-yellow-200", flipClasses);
     }
 
     if (isRevealed) {
       if (isMine) {
-        return cn(baseClasses, "bg-red-500 text-white");
+        return cn(baseClasses, "bg-red-500 text-white", flipClasses);
       }
-      return cn(baseClasses, "bg-gray-200");
+      return cn(baseClasses, "bg-gray-200", flipClasses);
     }
 
-    return cn(baseClasses, "bg-gray-300 hover:bg-gray-400");
+    return cn(baseClasses, "bg-gray-300 hover:bg-gray-400", flipClasses);
   };
 
   return (
